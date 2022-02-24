0f0c103645e54ce4f0fcbade8e8043cf7051c045
luisservera <lservera03@gmail.com>
2022-02-24 18:59:02 +0100

    Implemented players' imageviews and textviews of the header

--- app/src/main/java/com/luisangelservera/tres_raya/controller/GameActivity.java
+++ app/src/main/java/com/luisangelservera/tres_raya/controller/GameActivity.java
@@ -1,8 +1,10 @@
-package com.luisangelservera.tres_raya;
+package com.luisangelservera.tres_raya.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.luisangelservera.tres_raya.R;
 
 public class GameActivity extends AppCompatActivity {
 
