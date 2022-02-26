424c6c30c75cd7889e64bac47812198a14245e6f
luisservera <lservera03@gmail.com>
2022-02-26 11:41:50 +0100

    Implemented player turn label, error toast and save the info in matrix

--- app/src/main/java/com/luisangelservera/tres_raya/controller/GameActivity.java
+++ app/src/main/java/com/luisangelservera/tres_raya/controller/GameActivity.java
@@ -1,179 +1,272 @@
 package com.luisangelservera.tres_raya.controller;
 
+import androidx.annotation.IdRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.luisangelservera.tres_raya.R;
 
+import java.util.Arrays;
+
 public class GameActivity extends AppCompatActivity {
 
-    private int playerturn = 0;
-    boolean clicked1, clicked2, clicked3, clicked4, clicked5, clicked6, clicked7, clicked8, clicked9 = false;
+    private TextView playerTurnText;
+
+    private static final char EMPTY_CHAR = '-';
+    private static final char CROSS_CHAR = 'X';
+    private static final char CIRCLE_CHAR = 'O';
+
+    private char[][] matrix = new char[3][3];
+    private int playerTurn = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_activity);
 
+        fillMatrix();
+
+        playerTurnText = (TextView) findViewById(R.id.playerTurnTextView);
+        playerTurnText.setText(R.string.player1_turn_string);
+
 
         ImageButton button00 = (ImageButton) findViewById(R.id.button00);
         button00.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!clicked1) {
-                    if (playerturn == 0) {
-                        cruz(R.id.button00);
-                        playerturn = 1;
+                if (checkPositionEmpty(0, 0)) {
+                    if (playerTurn == 0) {
+                        setCrossToButton(R.id.button00);
+                        setPositionInMatrix(0, 0, CROSS_CHAR);
+                        playerTurn = 1;
                     } else {
-                        circulo(R.id.button00);
-                        playerturn = 0;
+                        setCircleToButton(R.id.button00);
+                        setPositionInMatrix(0, 0, CIRCLE_CHAR);
+                        playerTurn = 0;
                     }
-                    clicked1 = true;
+
+                    changePlayerTurnLabel();
+                } else {
+                    showPositionError();
                 }
             }
         });
+
         ImageButton button01 = (ImageButton) findViewById(R.id.button01);
         button01.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!clicked2) {
-                    if (playerturn == 0) {
-                        cruz(R.id.button01);
-                        playerturn = 1;
+                if (checkPositionEmpty(0, 1)) {
+                    if (playerTurn == 0) {
+                        setCrossToButton(R.id.button01);
+                        setPositionInMatrix(0, 1, CROSS_CHAR);
+                        playerTurn = 1;
                     } else {
-                        circulo(R.id.button01);
-                        playerturn = 0;
+                        setCircleToButton(R.id.button01);
+                        setPositionInMatrix(0, 1, CIRCLE_CHAR);
+                        playerTurn = 0;
                     }
-                    clicked2 = true;
+
+                    changePlayerTurnLabel();
+                } else {
+                    showPositionError();
                 }
             }
         });
+
         ImageButton button02 = (ImageButton) findViewById(R.id.button02);
         button02.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!clicked3) {
-                    if (playerturn == 0) {
-                        cruz(R.id.button02);
-                        playerturn = 1;
+                if (checkPositionEmpty(0, 2)) {
+                    if (playerTurn == 0) {
+                        setCrossToButton(R.id.button02);
+                        setPositionInMatrix(0, 2, CROSS_CHAR);
+                        playerTurn = 1;
                     } else {
-                        circulo(R.id.button02);
-                        playerturn = 0;
+                        setCircleToButton(R.id.button02);
+                        setPositionInMatrix(0, 2, CIRCLE_CHAR);
+                        playerTurn = 0;
                     }
-                    clicked3 = true;
+
+                    changePlayerTurnLabel();
+                } else {
+                    showPositionError();
                 }
             }
         });
+
         ImageButton button10 = (ImageButton) findViewById(R.id.button10);
         button10.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!clicked4) {
-                    if (playerturn == 0) {
-                        cruz(R.id.button10);
-                        playerturn = 1;
+                if (checkPositionEmpty(1, 0)) {
+                    if (playerTurn == 0) {
+                        setCrossToButton(R.id.button10);
+                        setPositionInMatrix(1, 0, CROSS_CHAR);
+                        playerTurn = 1;
                     } else {
-                        circulo(R.id.button10);
-                        playerturn = 0;
+                        setCircleToButton(R.id.button10);
+                        setPositionInMatrix(1, 0, CIRCLE_CHAR);
+                        playerTurn = 0;
                     }
-                    clicked4 = true;
+
+                    changePlayerTurnLabel();
+                } else {
+                    showPositionError();
                 }
             }
         });
+
         ImageButton button11 = (ImageButton) findViewById(R.id.button11);
         button11.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!clicked5) {
-                    if (playerturn == 0) {
-                        cruz(R.id.button11);
-                        playerturn = 1;
+                if (checkPositionEmpty(1, 1)) {
+                    if (playerTurn == 0) {
+                        setCrossToButton(R.id.button11);
+                        setPositionInMatrix(1, 1, CROSS_CHAR);
+                        playerTurn = 1;
                     } else {
-                        circulo(R.id.button11);
-                        playerturn = 0;
+                        setCircleToButton(R.id.button11);
+                        setPositionInMatrix(1, 1, CIRCLE_CHAR);
+                        playerTurn = 0;
                     }
-                    clicked5 = true;
+
+                    changePlayerTurnLabel();
+                } else {
+                    showPositionError();
                 }
             }
         });
+
         ImageButton button12 = (ImageButton) findViewById(R.id.button12);
         button12.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!clicked6) {
-                    if (playerturn == 0) {
-                        cruz(R.id.button12);
-                        playerturn = 1;
+                if (checkPositionEmpty(1, 2)) {
+                    if (playerTurn == 0) {
+                        setCrossToButton(R.id.button12);
+                        setPositionInMatrix(1, 2, CROSS_CHAR);
+                        playerTurn = 1;
                     } else {
-                        circulo(R.id.button12);
-                        playerturn = 0;
+                        setCircleToButton(R.id.button12);
+                        setPositionInMatrix(1, 2, CIRCLE_CHAR);
+                        playerTurn = 0;
                     }
-                    clicked6 = true;
+
+                    changePlayerTurnLabel();
+                } else {
+                    showPositionError();
                 }
             }
         });
+
         ImageButton button20 = (ImageButton) findViewById(R.id.button20);
         button20.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!clicked7) {
-                    if (playerturn == 0) {
-                        cruz(R.id.button20);
-                        playerturn = 1;
+                if (checkPositionEmpty(2, 0)) {
+                    if (playerTurn == 0) {
+                        setCrossToButton(R.id.button20);
+                        setPositionInMatrix(2, 0, CROSS_CHAR);
+                        playerTurn = 1;
                     } else {
-                        circulo(R.id.button20);
-                        playerturn = 0;
+                        setCircleToButton(R.id.button20);
+                        setPositionInMatrix(2, 0, CIRCLE_CHAR);
+                        playerTurn = 0;
                     }
-                    clicked7 = true;
+
+                    changePlayerTurnLabel();
+                } else {
+                    showPositionError();
                 }
             }
         });
+
         ImageButton button21 = (ImageButton) findViewById(R.id.button21);
         button21.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!clicked8) {
-                    if (playerturn == 0) {
-                        cruz(R.id.button21);
-                        playerturn = 1;
+                if (checkPositionEmpty(2, 1)) {
+                    if (playerTurn == 0) {
+                        setCrossToButton(R.id.button21);
+                        setPositionInMatrix(2, 1, CROSS_CHAR);
+                        playerTurn = 1;
                     } else {
-                        circulo(R.id.button21);
-                        playerturn = 0;
+                        setCircleToButton(R.id.button21);
+                        setPositionInMatrix(2, 1, CIRCLE_CHAR);
+                        playerTurn = 0;
                     }
-                    clicked8 = true;
+
+                    changePlayerTurnLabel();
+                } else {
+                    showPositionError();
                 }
             }
         });
+
         ImageButton button22 = (ImageButton) findViewById(R.id.button22);
         button22.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!clicked9) {
-                    if (playerturn == 0) {
-                        cruz(R.id.button22);
-                        playerturn = 1;
+                if (checkPositionEmpty(2, 2)) {
+                    if (playerTurn == 0) {
+                        setCrossToButton(R.id.button22);
+                        setPositionInMatrix(2, 2, CROSS_CHAR);
+                        playerTurn = 1;
                     } else {
-                        circulo(R.id.button22);
-                        playerturn = 0;
+                        setCircleToButton(R.id.button22);
+                        setPositionInMatrix(2, 2, CIRCLE_CHAR);
+                        playerTurn = 0;
                     }
-                    clicked9 = true;
+
+                    changePlayerTurnLabel();
+                } else {
+                    showPositionError();
                 }
             }
         });
     }
 
-    public void cruz(int button) {
+    private void setCrossToButton(@IdRes int button) {
         ImageButton ima1 = (ImageButton) findViewById(button);
         ima1.setImageResource(R.drawable.outline_close_black_48);
     }
 
-    public void circulo(int button) {
+    private void setCircleToButton(@IdRes int button) {
         ImageButton ima1 = (ImageButton) findViewById(button);
         ima1.setImageResource(R.drawable.outline_radio_button_unchecked_black_48);
     }
+
+    private void changePlayerTurnLabel() {
+        if (playerTurn == 0) {
+            playerTurnText.setText(R.string.player1_turn_string);
+        } else {
+            playerTurnText.setText(R.string.player2_turn_string);
+        }
+    }
+
+    private void setPositionInMatrix(int x, int y, char character) {
+        matrix[x][y] = character;
+    }
+
+    private boolean checkPositionEmpty(int x, int y) {
+        return matrix[x][y] == EMPTY_CHAR;
+    }
+
+    private void fillMatrix() {
+        for (char[] row : matrix)
+            Arrays.fill(row, EMPTY_CHAR);
+    }
+
+    private void showPositionError() {
+        Toast.makeText(GameActivity.this, "This position is not empty!", Toast.LENGTH_SHORT).show();
+    }
+
 }
